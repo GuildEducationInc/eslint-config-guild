608054b3cc09f5d0b41947c85fae208dea2703f1
James Olson <sixfootsixdesigns@gmail.com>
2021-03-24 16:11:15 -0600

    add testing library linter

--- packages/eslint-config-guild-typescript/index.js
+++ packages/eslint-config-guild-typescript/index.js
@@ -30,7 +30,8 @@ module.exports = {
     "jsx-a11y",
     "chai-friendly",
     "react-hooks",
-    "jest"
+    "jest",
+    "testing-library"
   ],
   "globals": {
     "document": false,
@@ -45,6 +46,7 @@ module.exports = {
     "plugin:jsx-a11y/recommended",
     "plugin:react/recommended",
     "plugin:jest/recommended",
+    "plugin:testing-library/recommended",
     "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
